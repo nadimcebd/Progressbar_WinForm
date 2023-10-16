401d7bb3e101e2bf67b9203fe2f8bbaa5c4bccf7
nadim <nazmul.alom@tblbd.com>
2023-10-16 11:31:02 +0600

    Progressbar

---
+++ ProgressBar_WinForm/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgressBar_WinForm
+{
+    public partial class Form1 : System.Windows.Forms.Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            backgroundWorker1.WorkerReportsProgress = true;
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                // Wait 100 milliseconds.  
+                Thread.Sleep(50);
+                // Report progress.  
+                backgroundWorker1.ReportProgress(i);
+            }
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            // Change the value of the ProgressBar  
+            progressBar1.Value = e.ProgressPercentage;
+            // Set the text.  
+            this.Text = "Progress: " + e.ProgressPercentage.ToString() + "%";
+        }
+    }
+}
